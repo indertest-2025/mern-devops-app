73cf7b6f42d145efaae62e6598355f9f17c8ac85
Inderjit Singh <indertest.2025@gmail.com>
2026-05-08 07:14:38 +0000

    Change done

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/api/test", (req, res) => {
   res.json({
-    message: "Backend API Working fine now"
+    message: "Backend API Working"
   });
 });
 
