162e7193753f843d6c1463b7b4b189773955026b
Inderjit Singh <indertest.2025@gmail.com>
2026-05-08 08:13:27 +0000

    Changes done

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,12 @@ app.get("/api/test", (req, res) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running"
+  });
+});
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
